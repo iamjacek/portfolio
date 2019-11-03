0cb2fc0423fbff15e9e78b8d633289b43fb3f01b
iamjacek <iamjacek@icloud.com>
2019-11-03 19:58:35 +0000

    add TimelineMax inport from gsap

--- src/components/Sections/Contact.js
+++ src/components/Sections/Contact.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-
+import { TimelineMax } from 'gsap';
 import Form from '../Form/Form';
 import Icon from '../Icon/Icon';
 
