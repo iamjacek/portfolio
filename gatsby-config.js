ca90684daa48147ccb720021ab532bdbd91633c0
iamjacek <iamjacek@icloud.com>
2020-04-08 12:07:30 +0100

    favicon amendmends

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,23 +6,29 @@ module.exports = {
     siteUrl: `https://www.jacekwitucki.com`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: './src/assets/favicon.png',
+
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          yandex: false,
+          windows: false,
+        },
+      },
+    },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-sitemap`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Jacek Witucki Web Development`,
-        short_name: `JW`,
-        start_url: `/`,
-        background_color: `#545353`,
-        theme_color: `#F0EBE7`,
-        display: `standalone`,
-        icon: 'src/assets/favicon.png',
-      },
-    },
+
     // this (optionalProgressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
